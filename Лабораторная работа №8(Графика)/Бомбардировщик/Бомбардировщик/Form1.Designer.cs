9c71d1cf814f785e76f9af9e7581bab4e6a7da26
Dmitriy Fursov <19dima991999@mail.ru>
2017-12-11 00:18:33 +0300

    Pre-alpha

---
+++ Лабораторная работа №8(Графика)/Бомбардировщик/Бомбардировщик/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Бомбардировщик
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(650, 558);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(650, 557);
+            this.Controls.Add(this.pictureBox1);
+            this.KeyPreview = true;
+            this.MaximumSize = new System.Drawing.Size(666, 595);
+            this.MinimumSize = new System.Drawing.Size(666, 595);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Бомбардировщик";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Лабораторная работа №8(Графика)/Бомбардировщик/Бомбардировщик/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Бомбардировщик
+{
+    public struct EnemyObject
+    {
+        public Image castle;
+        public int xCastle;
+        public int yCastle;
+        public bool destroy;
+    }
+    public partial class Form1 : Form
+    {
+        Image background, plane, zoom, cloud;
+        BufferedGraphicsContext currentContext;
+        BufferedGraphics myBuffer;
+        Timer timer;
+        Random r = new Random();
+        int xPlain = 240, yPlain = 420, xBack = 500, yBack = 2500, xZoom = 290, yZoom = 300, xCloud, yCloud, score = 0;
+        EnemyObject[] enemy = new EnemyObject[4];
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.A && xPlain > 0)
+                xPlain-=3;
+            if (e.KeyCode == Keys.D && xPlain < 455)
+                xPlain += 3;
+            if (e.KeyCode == Keys.Space)
+            {
+                for (int i = 0; i < enemy.Length; i++)
+                {
+                    if (xZoom + 33 >= enemy[i].xCastle - 30 &&
+                        xZoom + 33 <= enemy[i].xCastle + 130 && yZoom + 33 >= enemy[i].yCastle - 100 &&
+                        yZoom + 33 <= enemy[i].yCastle + 50)
+                    {
+                        enemy[i].destroy = true;
+                        score += i;
+                    }
+                }
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            plane = Image.FromFile("plane.png");
+            zoom = Image.FromFile("zoom.png");
+            background = Image.FromFile("background2.jpg");
+            cloud = Image.FromFile("cloud.png");
+            xCloud = r.Next(50, 400);
+            yCloud = r.Next(-400, -100);
+            int tmpX, tmpY;
+            for (int i = 0; i < enemy.Length; i++)
+            {
+                enemy[i].castle = Image.FromFile("castle" + i + ".png");
+                enemy[i].destroy = false;
+                do
+                {
+                    tmpX = r.Next(50, 400);
+                    tmpY = r.Next(-400, -100);
+                } while (!FreePlace(tmpX, tmpY));
+                enemy[i].xCastle = tmpX;
+                enemy[i].yCastle = tmpY;
+            }
+            timer = new Timer();
+            timer.Interval = 1;
+            timer.Enabled = true;
+            timer.Tick += new EventHandler(timer1_Tick);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            currentContext = BufferedGraphicsManager.Current;
+            myBuffer = currentContext.Allocate(this.CreateGraphics(), this.DisplayRectangle);
+            myBuffer.Graphics.DrawImage(background, 0, 0, new Rectangle(xBack, yBack, 650, 558), GraphicsUnit.Pixel);
+            CreateAndMoveEnemy();
+            myBuffer.Graphics.DrawImage(zoom, xZoom, yZoom);
+            myBuffer.Graphics.DrawImage(plane, xPlain, yPlain);
+            myBuffer.Graphics.DrawString("A-left\nD-right\nSpace-fire", new Font("Times New Roman", 30), Brushes.Black, 10, 10);
+            myBuffer.Graphics.DrawString("Score: " + score, new Font("Times New Roman", 30), Brushes.Black, 450, 20);
+            if (yCloud >= 600)
+            {
+                xCloud = r.Next(50, 400);
+                yCloud = r.Next(-400, -100);
+            }
+            myBuffer.Graphics.DrawImage(cloud, xCloud, yCloud);
+            xZoom = xPlain + 50;
+            myBuffer.Render(pictureBox1.CreateGraphics());
+            if (yBack < 500)
+                yBack = 2500;
+            yBack--;
+            yCloud++;
+            myBuffer.Dispose();
+            if (xPlain + 30 >= xCloud - 20 && xPlain + 30 <= xCloud + 80 && yPlain >= yCloud - 20 && yPlain <= yCloud + 20)
+            {
+                timer.Enabled = false;
+                MessageBox.Show("Вы проиграли! Ваш счет: " + score);
+                this.Close();
+                //this.Hide();
+                //Form2 form2 = new Form2();
+                //form2.ShowDialog();
+            }
+
+        }
+        bool FreePlace(int x, int y)
+        {
+            bool free = true;
+            for (int i = 0; i < enemy.Length; i++)
+            {
+                if (x >= enemy[i].xCastle - 100 &&
+                        x <= enemy[i].xCastle + 180 && y >= enemy[i].yCastle - 120 &&
+                        y <= enemy[i].yCastle + 200)
+                    free = false;
+                    
+            }
+            return free;
+        }
+        void CreateAndMoveEnemy()
+        {
+            for (int i = 0; i < enemy.Length; i++)
+            {
+                if (enemy[i].destroy)
+                {
+                    enemy[i].destroy = false;
+                    int tmpX, tmpY;
+                    do
+                    {
+                        tmpX = r.Next(50, 400);
+                        tmpY = r.Next(-400, -100);
+                    } while (!FreePlace(tmpX, tmpY));
+                    enemy[i].xCastle = tmpX;
+                    enemy[i].yCastle = tmpY;
+                }
+                if (enemy[i].yCastle < 650 && enemy[i].destroy == false)
+                {
+                    myBuffer.Graphics.DrawImage(enemy[i].castle, enemy[i].xCastle, enemy[i].yCastle - 80);
+                    enemy[i].yCastle++;
+                }
+                else
+                {
+                    enemy[i].destroy = true;
+                }
+            }
+        }
+    }
+}
